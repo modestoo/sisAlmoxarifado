de031696286feb4cdddc7fd489e90e31d5990950
Augusto Modesto <augusto.modesto@live.com>
2017-04-05 15:10:11 -0300

    Atualização das models

--- sisAlmoxarifado/recebimentoMaterial/models.py
+++ sisAlmoxarifado/recebimentoMaterial/models.py
@@ -3,21 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Material(models.Model):
-	nome_material = models.CharField(max_length=200)
-	tipo_material = models.CharField(max_length=200)
-	unidade_medida = models.CharField(max_length=200)	
+	nome_material = models.CharField(max_length=200, null=False)
+	tipo_material = models.CharField(max_length=200, null=False)
+	unidade_medida = models.CharField(max_length=200, null=False)	
 	
 	def __str__(self):
 		return self.nome_material
 
-class SetorRequisitante(models.Model):
-	nome_setorRequisitante = models.CharField(max_length=200)
-
-	def __str__(self):
-		return self.nome_setorRequisitante
-
 class Estoque(models.Model):
-	quantidade_material = models.IntegerField()
+	quantidade_material = models.IntegerField(null=False)
 	materiais = models.ForeignKey(Material, on_delete=models.CASCADE)
 
 	def __str__(self):
@@ -25,6 +19,6 @@ class Estoque(models.Model):
 
 class RecebimentoMaterial(models.Model):
 	responsavel_pelo_recebimento = models.CharField(max_length=200)
-	data_recebimento = models.DateTimeFields(blank=True, null=True)
+	data_recebimento = models.DateTimeField(blank=True, null=False)
 
 
